235b53cbf24ec515dca7db1aa38f79622c361f62
Dennis Eichnau <dennis@eichnau.eu>
2026-03-24 11:26:37 +0100

    Pokedex version 1.0

---
+++ render.js
@@ -0,0 +1,23 @@
+function renderPokemon() {
+    const POKEMON_CONTAINER = document.getElementById("pokemon");
+    for (let index = 0; index < allPokemon.length; index++) {
+      POKEMON_CONTAINER.innerHTML += getPokemonTemplate(index);
+      checkSecondType(index);
+    }
+  }
+
+  function renderOverlayPokemon(index) {
+    let currentPokemon = document.getElementById("overlay-content");
+    currentPokemon.innerHTML = getOverlayPokemonTemplate(index);
+  }
+
+  function renderFilteredPokemon(filteredPokemon) {
+    const POKEMON_CONTAINER = document.getElementById("pokemon");
+    if(filteredPokemon.length == 0){
+      POKEMON_CONTAINER.innerHTML = getNotFilteredPokemonTemplate();
+    }
+    for (let index = 0; index < filteredPokemon.length; index++) {
+      POKEMON_CONTAINER.innerHTML += getFilteredPokemonTemplate(index);
+      checkSecondType(index);
+    }
+  }
---
+++ script.js
@@ -0,0 +1,114 @@
+let nextPokemonUrl = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20";
+let pokemonList = [];
+let allPokemon = [];
+let filteredPokemon = [];
+
+async function init() {
+  loadingSpinner()
+  await fetchPokemonList(); 
+}
+
+async function fetchPokemonList() {
+  let response = await fetch(nextPokemonUrl);
+  let reseponseAsJson = await response.json();
+  getPokemonForList(reseponseAsJson);
+  nextPokemonUrl = reseponseAsJson.next;
+  loadingSpinner()
+}
+
+async function getPokemonForList(pokeList) {
+  pokemonList = pokeList.results;
+  await getSinglePokemonData(pokemonList);
+}
+
+async function getSinglePokemonData(pokemonList) {
+  for (
+    let pokemonIndex = 0;
+    pokemonIndex < pokemonList.length;
+    pokemonIndex++
+  ) {
+    const RESPONSE = await fetch(pokemonList[pokemonIndex].url);
+    const SINGLE_POKEMON = await RESPONSE.json();
+    allPokemon.push(SINGLE_POKEMON);
+  }
+  renderPokemon();
+  disabledLoadingSpinner()
+}
+
+function checkSecondType(index) {
+  const SECOND_TYPE = allPokemon[index].types[1];
+  const TYPE_CONTAINER = document.getElementById(`pokemon-types-${index}`);
+  if (SECOND_TYPE) {
+    TYPE_CONTAINER.innerHTML += `<img class="${SECOND_TYPE.type.name}" src="./assets/icons-type/${SECOND_TYPE.type.name}.svg"> `;
+  }
+}
+
+function pokemonOverlayOn(index) {
+  document.getElementById("overlay").style.display = "flex";
+  document.getElementById("overlay-content").style.display = "flex";
+  renderOverlayPokemon(index);
+  document.getElementById("body").classList.add("no-scroll");
+}
+
+function pokemonOverlayOff() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("overlay-content").style.display = "none";
+  document.getElementById("body").classList.remove("no-scroll");
+}
+
+function searchPokemon() {
+  const POKEMON_CONTAINER = document.getElementById("pokemon");
+    POKEMON_CONTAINER.innerHTML = "";
+  let searchInput = document.getElementById("search").value.toLowerCase();
+  if (searchInput.length >= 2) {
+    filteredPokemon = allPokemon.filter((pokemon) => {return pokemon.name.includes(searchInput);});
+    renderFilteredPokemon(filteredPokemon);
+    document.getElementById("myPopup").classList.add("d_none")
+    btnDnone()
+    }else {
+    renderPokemon();
+    popupSearchInput()
+    btnBlock()
+  }
+}
+
+async function loadNextPokemon() {
+  nextPokemonUrl;
+  const POKEMON_CONTAINER = document.getElementById("pokemon");
+  POKEMON_CONTAINER.innerHTML = "";
+  fetchPokemonList();
+  disabledLoadingSpinner()
+}
+
+function currentOverlayRight(index) {
+  indexAll = allPokemon.length;
+  index = (index + indexAll) % indexAll
+  renderOverlayPokemon(index);
+}
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+function loadingSpinner() {
+ document.getElementById("loader").style.display = "flex"
+}
+
+function disabledLoadingSpinner(){
+document.getElementById("loader").style.display = "none"
+}
+
+function popupSearchInput() {
+  const popup = document.getElementById("myPopup");
+  popup.classList.add("show");
+}
+
+function btnDnone(change) {
+  const button = document.getElementById("btn")
+  button.classList.add("d_none")
+}
+
+function btnBlock() {
+  const button = document.getElementById("btn")
+  button.classList.remove("d_none")
+}
